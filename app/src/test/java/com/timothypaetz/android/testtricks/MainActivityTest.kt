9d7997f62cecdb0773014c9265ea325b4649c64d
Timothy Paetz <paetztm@gmail.com>
2023-01-01 09:30:14 -0800

    Initial unit tests showing how to test a public function in an activity

--- app/src/test/java/com/timothypaetz/android/testtricks/MainActivityTest.kt
+++ app/src/test/java/com/timothypaetz/android/testtricks/MainActivityTest.kt
@@ -2,14 +2,34 @@ package com.timothypaetz.android.testtricks
 
 import org.junit.Assert.*
 import org.junit.Test
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
+import org.mockito.kotlin.mock
 
 class MainActivityTest {
 
-    private val mainActivity = mock(MainActivity::class.java)
+    @Test
+    fun `verify no name user returns false for hasCompletedProfile`() {
+        // given
+        val mainActivity: MainActivity = mock(defaultAnswer = CALLS_REAL_METHODS)
+        val noNameUser = User(name = null)
+
+        // when
+        val actual = mainActivity.hasCompletedProfile(noNameUser)
+
+        // then
+        assertFalse(actual)
+    }
 
     @Test
-    fun verifyMockitoWorks() {
-        assertNotNull(mainActivity)
+    fun `verify named user returns true for hasCompletedProfile`() {
+        // given
+        val mainActivity: MainActivity = mock(defaultAnswer = CALLS_REAL_METHODS)
+        val namedUser = User(name = "Timothy Paetz")
+
+        // when
+        val actual = mainActivity.hasCompletedProfile(namedUser)
+
+        // then
+        assertTrue(actual)
     }
 }
