d085b50f33a5981d5d1fb08c5f5ee794adcbf8ee
Timothy Paetz <tim@timothypaetz.com>
2023-08-13 16:07:29 -0700

    Added mockk as well for Trick 1

--- app/src/test/java/com/timothypaetz/android/androidtesttricks/Trick1ActivityTest.kt
+++ app/src/test/java/com/timothypaetz/android/androidtesttricks/Trick1ActivityTest.kt
@@ -1,5 +1,7 @@
 package com.timothypaetz.android.androidtesttricks
 
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.Assert.*
 import org.junit.Test
 import org.mockito.Mockito.*
@@ -7,6 +9,9 @@ import org.mockito.kotlin.mock
 
 class Trick1ActivityTest {
 
+    /**
+     * Mockito Example
+     */
     @Test
     fun `verify no name user returns false for hasCompletedProfile`() {
         // given
@@ -25,6 +30,37 @@ class Trick1ActivityTest {
         // given
         val trick1Activity: Trick1Activity = mock(defaultAnswer = CALLS_REAL_METHODS)
         val namedUser = User(name = "Timothy Paetz")
+
+        // when
+        val actual = trick1Activity.hasCompletedProfile(namedUser)
+
+        // then
+        assertTrue(actual)
+    }
+
+    /**
+     * Mockk example
+     */
+    @Test
+    fun `verify no name user returns false for hasCompletedProfile using mockk`() {
+        // given
+        val trick1Activity = mockk<Trick1Activity>()
+        val noNameUser = User(name = null)
+        every { trick1Activity.hasCompletedProfile(noNameUser) } answers { callOriginal() }
+
+        // when
+        val actual = trick1Activity.hasCompletedProfile(noNameUser)
+
+        // then
+        assertFalse(actual)
+    }
+
+    @Test
+    fun `verify named user returns true for hasCompletedProfile using mockk`() {
+        // given
+        val trick1Activity = mockk<Trick1Activity>()
+        val namedUser = User(name = "Timothy Paetz")
+        every { trick1Activity.hasCompletedProfile(namedUser) } answers { callOriginal() }
 
         // when
         val actual = trick1Activity.hasCompletedProfile(namedUser)
